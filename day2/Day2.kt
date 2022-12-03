faaf7d442adaf5c1ae191b620a6e853cd80c1eee
jmkraut <jacobmatthewkraut@hotmail.com>
2022-12-03 16:04:42 +1000

    Tried playing around with enums, maybe a bit too much. Could do with some refactoring, I feel like there is a lot of repetition that can be cleaned up. Might be a bit over-engineered.

--- day2/Day2.kt
+++ day2/Day2.kt
@@ -6,50 +6,67 @@ fun day2Solution () {
     val filename = "day2/resources/d2_input.txt"
     val testFilename = "day2/resources/d2_input_test.txt"
 
-    var totalPoints = 0
+    var totalPointsPart1 = 0
+    var totalPointsPart2 = 0
 
     File(filename).forEachLine {
         val gameChars = it.toCharArray()
-        totalPoints += (getOutcomeOfRound(gameChars[0], gameChars[2]).Points + StrategyMove.valueOf(gameChars[2].toString()).Play.Points)
+        val player1Move = StrategyMove.valueOf(gameChars[0].toString())
+        val player2Move = StrategyMove.valueOf(gameChars[2].toString())
+
+        totalPointsPart1 += getPointsForRoundPart1(player1Move, player2Move)
+        totalPointsPart2 += getPointsForRoundPart2(player1Move, player2Move)
     }
     println("~~~ Day 2 ~~~")
-    println("Part 1 ~ Total points for first game: $totalPoints")
+    println("Part 1 ~ Total points for first game: $totalPointsPart1")
+    println("Part 1 ~ Total points for first game: $totalPointsPart2")
     println("~~~~~~~~~~~~~")
 }
 
-fun getOutcomeOfRound(player1Move: Char, player2Move: Char): GameOutcome {
-    if(StrategyMove.valueOf(player1Move.toString()).Play == Moves.ROCK
-        && StrategyMove.valueOf(player2Move.toString()).Play == Moves.SCISSORS ) {
-        return GameOutcome.LOSE
-    } else if (StrategyMove.valueOf(player1Move.toString()).Play == Moves.SCISSORS
-        && StrategyMove.valueOf(player2Move.toString()).Play == Moves.PAPER ) {
-        return GameOutcome.LOSE
-    } else if (StrategyMove.valueOf(player1Move.toString()).Play == Moves.PAPER
-        && StrategyMove.valueOf(player2Move.toString()).Play == Moves.ROCK ){
-        return GameOutcome.LOSE
-    } else if (StrategyMove.valueOf(player1Move.toString()).Play == StrategyMove.valueOf(player2Move.toString()).Play) {
-        return GameOutcome.DRAW
-    }
+fun getPointsForRoundPart1(player1Move: StrategyMove, player2Move: StrategyMove): Int {
+    if(player1Move.play == Move.ROCK && player2Move.play == Move.PAPER) return GameOutcome.WIN.points + player2Move.play.points
+    if(player1Move.play == Move.PAPER && player2Move.play == Move.SCISSORS) return GameOutcome.WIN.points + player2Move.play.points
+    if(player1Move.play == Move.SCISSORS && player2Move.play == Move.ROCK) return GameOutcome.WIN.points + player2Move.play.points
 
-    return GameOutcome.WIN
+    if(player1Move.play == Move.ROCK && player2Move.play == Move.SCISSORS) return GameOutcome.LOSE.points + player2Move.play.points
+    if(player1Move.play == Move.PAPER && player2Move.play == Move.ROCK) return GameOutcome.LOSE.points + player2Move.play.points
+    if(player1Move.play == Move.SCISSORS && player2Move.play == Move.PAPER) return GameOutcome.LOSE.points + player2Move.play.points
+
+    return GameOutcome.DRAW.points + player2Move.play.points
 }
 
-enum class StrategyMove(val Play: Moves) {
-    A(Moves.ROCK),
-    B(Moves.PAPER),
-    C(Moves.SCISSORS),
-    X(Moves.ROCK),
-    Y(Moves.PAPER),
-    Z(Moves.SCISSORS),
+fun getPointsForRoundPart2(player1Move: StrategyMove, player2Move: StrategyMove): Int {
+    if(player1Move.play == Move.ROCK && player2Move == StrategyMove.X) return GameOutcome.LOSE.points + Move.SCISSORS.points
+    if(player1Move.play == Move.ROCK && player2Move == StrategyMove.Y) return GameOutcome.DRAW.points + Move.ROCK.points
+    if(player1Move.play == Move.ROCK && player2Move == StrategyMove.Z) return GameOutcome.WIN.points + Move.PAPER.points
+
+    if(player1Move.play == Move.PAPER && player2Move == StrategyMove.X) return GameOutcome.LOSE.points + Move.ROCK.points
+    if(player1Move.play == Move.PAPER && player2Move == StrategyMove.Y) return GameOutcome.DRAW.points + Move.PAPER.points
+    if(player1Move.play == Move.PAPER && player2Move == StrategyMove.Z) return GameOutcome.WIN.points + Move.SCISSORS.points
+
+    if(player1Move.play == Move.SCISSORS && player2Move == StrategyMove.X) return GameOutcome.LOSE.points + Move.PAPER.points
+    if(player1Move.play == Move.SCISSORS && player2Move == StrategyMove.Y) return GameOutcome.DRAW.points + Move.SCISSORS.points
+    if(player1Move.play == Move.SCISSORS && player2Move == StrategyMove.Z) return GameOutcome.WIN.points + Move.ROCK.points
+
+    return 0
 }
 
-enum class Moves(val Points: Int) {
+enum class StrategyMove(val play: Move) {
+    A(Move.ROCK),
+    B(Move.PAPER),
+    C(Move.SCISSORS),
+    X(Move.ROCK),
+    Y(Move.PAPER),
+    Z(Move.SCISSORS),
+}
+
+enum class Move(val points: Int) {
     ROCK(1),
     PAPER(2),
     SCISSORS(3)
 }
 
-enum class GameOutcome(val Points: Int) {
+enum class GameOutcome(val points: Int) {
     WIN(6),
     DRAW(3),
     LOSE(0)
