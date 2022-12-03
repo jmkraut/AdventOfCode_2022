d63f150dcdf07b8c8467f7c7d4e020bf611ebb2f
jmkraut <jacobmatthewkraut@hotmail.com>
2022-12-03 18:27:34 +1000

    Added a readme for day3 which I'll do for the rest of the days. Solution for day3 part1.

--- Main.kt
+++ Main.kt
@@ -1,7 +1,9 @@
 import day1.day1Solution
 import day2.day2Solution
+import day3.day3Solution
 
 fun main() {
     day1Solution()
     day2Solution()
+    day3Solution()
 }
--- day3/Day3.kt
+++ day3/Day3.kt
@@ -3,15 +3,42 @@ package day3
 import java.io.File
 
 fun day3Solution () {
-    val filename = "day2/resources/d2_input.txt"
-    val testFilename = "day2/resources/d2_input_test.txt"
+    val filename = "day3/resources/d3_input.txt"
+    val testFilename = "day3/resources/d3_input_test.txt"
 
+    var rucksackPrioritiesSum = 0
 
-    File(filename).forEachLine {
+    File(filename).forEachLine { it ->
+        val rucksack1 = (it.substring(0 , it.length / 2)).toCharArray()
+        val rucksack2 = (it.substring(it.length / 2, it.length)).toCharArray()
 
+        val result = rucksack1.first {
+            isCharValueInCharArray(it, rucksack2)
+        }
+
+        rucksackPrioritiesSum += getCharPriorityValue(result)
     }
+
     println("~~~ Day 3 ~~~")
-    println("Part 1 ~ Total points for first game: ")
-    println("Part 2 ~ Total points for first game: ")
+    println("Part 1 ~ Sum of priorities of the rucksacks: $rucksackPrioritiesSum")
     println("~~~~~~~~~~~~~")
+}
+
+fun isCharValueInCharArray(characterToCheck: Char, arrayToCheck: CharArray): Boolean {
+    for(char in arrayToCheck) {
+        if(char == characterToCheck) {
+            return true
+        }
+    }
+    return false
+}
+
+fun getCharPriorityValue(char: Char): Int {
+    val lowercaseCharPriorityCorrectionValue = 96
+    val uppercaseCharPriorityCorrectionValue = 38
+
+    if(char.isUpperCase()) {
+        return char.code - uppercaseCharPriorityCorrectionValue
+    }
+    return char.code - lowercaseCharPriorityCorrectionValue
 }
